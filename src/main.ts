c0a1431e5e3dd4e26a27e917e33996aee8a01ca6
Maksim Shuklun <makshuk@bk.ru>
2023-11-20 06:40:34 +0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const port = 3001;
 	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: ['http://localhost:3000'],
+		methods: ['GET', 'POST'],
+		credentials: true,
+	});
 	await app.listen(port);
 	console.log(`DB-API: ${port}`);
 }
